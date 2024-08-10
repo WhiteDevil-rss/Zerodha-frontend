08b5dfc68c8c8c5155e784aa36cb6b3af87418a3
Rajput Shivam Singh <ss7051017@gmail.com>
2024-08-10 11:14:11 +0530

    complete support page

--- src/landing_page/support/CreateTicket.jsx
+++ src/landing_page/support/CreateTicket.jsx
@@ -1,8 +1,78 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function CreateTicket() {
     return (
-        <h1>CreateTicket</h1>
+        <div className="container p-5">
+            <header className='p-5'>
+                <h4 className='fw-normal text-12'>To create a ticket, select a relevant topic</h4>
+            </header>
+            <div className="row px-5">
+                    <div className="col-lg-4 col-sm-12 fw-light text-12 mb-5">
+                        <h5 className='mb-3'><i class="fa-solid fa-circle-plus"></i> Account Opening</h5>
+                        <div className='px-3'>
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Getting started</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Online</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Offline</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Charges</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Company, Partnership and HUF</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Non Resident Indian (NRI)</Link><br />
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-12 fw-light text-12 mb-5">
+                        <h5 className='mb-3'><i class="fa-regular fa-user"></i> Your Zerodha Account</h5>
+                        <div className='px-3'>
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Login credentials</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Your Profile</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Account modification and segment addition</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>CMR & DP ID</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Nomination</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Transfer and conversion of shares</Link><br />
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-12 fw-light text-12 mb-5">
+                        <h5 className='mb-3'><i class="fa-solid fa-chart-simple"></i> Trading and Markets</h5>
+                        <div className='px-3'>
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Trading FAQs</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Kite</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Margins</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Product and order types</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Corporate actions</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Kite features</Link><br />
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-12 fw-light text-12 mb-5">
+                        <h5 className='mb-3'><i class="fa-regular fa-credit-card"></i> Funds</h5>
+                        <div className='px-3'>
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Fund withdrawal</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Adding funds</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Adding bank accounts</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>eMandates</Link><br />
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-12 fw-light text-12 mb-5">
+                        <h5 className='mb-3'><i class="fa-solid fa-chart-pie"></i> Console</h5>
+                        <div className='px-3'>
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>IPO</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Portfolio</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Funds statement</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Profile</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Reports</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Referral program</Link><br />
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-12 fw-light text-12 mb-5">
+                        <h5 className='mb-3'><i class="fa-solid fa-coins"></i> Coin</h5>
+                        <div className='px-3'>
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Understanding mutual funds and Coin</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Coin app</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Coin web</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>Transactions and reports</Link><br />
+                            <Link to="/support" style={{ textDecoration: "none" }} className='lh-lg'>National Pension Scheme (NPS)F</Link><br />
+                        </div>
+                    </div>
+            </div>
+        </div>
     );
 }
 
--- src/landing_page/support/Hero.jsx
+++ src/landing_page/support/Hero.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+    return (
+        <section className='container-fluid' style={{ backgroundColor: "#397DD1", color: "white" }}>
+            <div className="container">
+                <div className="row p-5">
+                    <div className="d-flex justify-content-between px-5 lh-lg">
+                        <h4 className='text-12'>Support Portal</h4>
+                        <Link to="/support" className='text-white'>Track tickets</Link>
+                    </div>
+                </div>
+                <div className="row px-4 mx-5">
+                    <div className="col-lg-7 col-sm-12">
+                        <h4 className='lh-lg fw-normal'>Search for an answer or browse help topics to create a ticket</h4>
+                        <form action="/">
+                            <div class="input-group mb-3 mt-3">
+                                <input type="text" className="form-control form-control-lg" style={{ fontSize: "16px", lineHeight: "36px" }} placeholder="Eg: how do i activate F&O, why is my order getting rejected ..." aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" autoFocus />
+                                <span class="input-group-text" id="inputGroup-sizing-lg"><button type='submit' style={{ border: "none" }}><i class="fa-solid fa-magnifying-glass fa-lg"></i></button></span>
+                            </div>
+                        </form>
+                        <div class="row mb-5">
+                            <Link to="/support" className='text-white col-lg-4 col-sm-12 lh-lg'><p>Track account opening</p></Link>
+                            <Link to="/support" className='text-white col-lg-4 col-sm-12 lh-lg'><p>Track segment activation</p></Link>
+                            <Link to="/support" className='text-white col-lg-4 col-sm-12 lh-lg'><p>Intraday margins</p></Link>
+                            <Link to="/support" className='text-white col-lg-4 col-sm-12 lh-lg mb-5'><p>Kite user manual</p></Link>
+                        </div>
+                    </div>
+                    <div className="col-lg-5 col-sm-12">
+                        <section className='ms-3'>
+                            <h4 className='mt-5'>Featured</h4>
+                            <ol className='lh-l'>
+                                <li className='m-2'>
+                                    <Link to="/support" className='text-white col-lg-4 col-sm-12 lh-lg'>Surveillance measure on scrips - August 2024</Link>
+                                </li>
+                                <li className='m-2'>
+                                    <Link to="/support" className='text-white col-lg-4 col-sm-12 lh-lg'>Latest Intraday leverages and Square-off timings</Link>
+                                </li>
+                            </ol>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Hero;
